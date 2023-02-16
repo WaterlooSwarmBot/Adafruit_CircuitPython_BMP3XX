1c37af4d49977134b2abe3cb879f04a833f1eb1b
Nick Shaju <nickshaju@gmail.com>
2023-02-16 17:56:22 -0500

    setup.py for local install

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adafruit-circuitpython-bmp3xx',
+    version='0.1.0',
+    packages=find_packages(include=['Adafruit_CircuitPython_BMP3XX', 'Adafruit_CircuitPython_BMP3XX.*'])
+)
